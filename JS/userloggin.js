22b8c0e2cbd22a93092ed24b96c0a94c89fca9d0
Jairo B <jairock@outlook.com>
2024-07-04 13:37:11 -0600

    feat: fix erros loggin v1

--- JS/userloggin.js
+++ JS/userloggin.js
@@ -1,36 +1,54 @@
-let hasToken = localStorage.getItem('token');
+let hasToken = localStorage.getItem('token')
 console.log(hasToken);
 
-const logoutButton = document.getElementById('logout');
+
 const loginButton = document.querySelector('.btn-login');
 
-const manageLogoutBtn = () => {
-    if (hasToken) {
-        logoutButton.classList.remove('d-none'); 
-    } else {
+const validateSession = () => {
+    let loginCard = document.getElementById('login-card');
+    let logoutButton = document.getElementById('logout');
+    hasToken = localStorage.getItem('token');
+    if(!hasToken) {
+        loginCard.classList.remove('d-none');
         logoutButton.classList.add('d-none');
+    // } else if (hasToken) {
+    } else {
+        loginCard.classList.add('d-none');
+        logoutButton.classList.remove('d-none');
     }
 };
 
-manageLogoutBtn();
-
+// Agregar un evento de clic al botón de inicio de sesión
 loginButton.addEventListener('click', () => {
-    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
-    localStorage.setItem('token', token);
-    hasToken = localStorage.getItem('token'); // Actualizar hasToken
-    manageLogoutBtn();
-    //window.location.href = '../index.html';
+    validateSession(); // Validar la sesión al hacer clic en el botón de inicio de sesión
 });
 
-// Función para remover token, simular cierre de sesión
+// Llamada inicial para validar la sesión al cargar la página
+validateSession();
+loginButton.addEventListener('click', () => {
+    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+    localStorage.setItem('token', token);
+    window.location.href = 'index.html';
+    validateSession();
+});
+
+// funcion para remover token, simular cierre de sesion
 const logout = () => {
+    // Eliminar el token del localStorage
     localStorage.removeItem('token');
-    hasToken = localStorage.getItem('token'); // Actualizar hasToken
-    manageLogoutBtn();
+
+    // Actualizar hasToken después de eliminar el token
+    hasToken = localStorage.getItem('token');
+
+    // Validar la sesión nuevamente después de eliminar el token
+    validateSession();
 };
 
+// logout 
+const logoutButton = document.querySelector('.btn-logout');
 if (logoutButton) {
     logoutButton.addEventListener('click', () => {
         logout();
     });
-}
+};
+export { hasToken, validateSession, loginButton, logoutButton, logout };
